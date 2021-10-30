55abc270b8887ee9303e89e8d900179b11f6cfec
Ali <ialimagomed@mail.ru>
2021-10-31 00:00:02 +0300

    feat: added config parse #2

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -21,7 +21,9 @@ func main() {
 
 	r.Get("/hello", h.Hello)
 
-	log.Print("starting server")
+	path := cfg.Host + ":" + cfg.Port
+
+	log.Printf("starting server at %s", path)
 	err = http.ListenAndServe(":8080", r)
 	log.Print("shutting server down")
 	log.Fatal(err)
