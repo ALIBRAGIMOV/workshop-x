16d0b3670601683cd919569c1deb141a65462b15
Ali <ialimagomed@mail.ru>
2021-10-31 00:17:42 +0300

    fix: fixed deploy app in heroku #1

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,9 @@
+
 package config
 
 // Server config
 type Server struct {
 	Port string `yaml:"port" env:"PORT"`
-	Host string	`yaml:"host" env:"HOST" env-default:"0.0.0.0"`
+	Host string `yaml:"host" env:"HOST" env-default:"0.0.0.0"`
 }
 
