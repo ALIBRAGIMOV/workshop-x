e9302841a7c8974518b01d51c1af8c26fe6164d2
Ali <ialimagomed@mail.ru>
2021-10-30 20:49:53 +0300

    added handler

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,5 +1,19 @@
 package workshop
 
-func mian()  {
-	
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+	"workshop/internal/handler"
+)
+
+func main()  {
+	h = handler.NewHandler()
+	r = chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
+
 }
