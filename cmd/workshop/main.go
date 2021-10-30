c7cea3cc1b71ea7cf797f384156f280ab7c22e59
Ali <ialimagomed@mail.ru>
2021-10-30 20:51:24 +0300

    fix: fixed main.gp

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,4 +1,4 @@
-package workshop
+package main
 
 import (
 	"github.com/go-chi/chi/v5"
@@ -8,8 +8,8 @@ import (
 )
 
 func main()  {
-	h = handler.NewHandler()
-	r = chi.NewRouter()
+	h := handler.NewHandler()
+	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
 
